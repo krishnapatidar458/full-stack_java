7ad22ba2b6cfdfd4460aefdbbde99d118d3f9f40
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-18 21:51:46 +0530

    understanding about variables and console/console.table()

---
+++ variables.js
@@ -0,0 +1,11 @@
+const rollNo = 458336;
+let email =  "test@gmail.com"
+var  password = "123456"
+city = "OLdBhopal"
+
+email = "test2@gmail.com"
+password = "987654"
+city = "NewBhopal"
+
+console.log(rollNo);
+console.table([rollNo,email,password,city]);
