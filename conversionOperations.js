2e1177cf1c3359712434215e99a532da1b47d9c9
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-19 20:01:17 +0530

    this is video 6 and i learn about the type converion

---
+++ conversionOperations.js
@@ -0,0 +1,21 @@
+let score = "krishna"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+
+let log = ""
+
+let newlog = Boolean(log)
+console.log(newlog);
+
+let somenum = 44
+let stringnum = String(somenum)
+
+console.log(typeof stringnum);
+console.log(stringnum);
