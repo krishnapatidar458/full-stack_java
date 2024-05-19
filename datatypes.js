e3a9f0324355c835ebadc91df70b8206fb1070c3
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-19 19:39:18 +0530

    videp 4 datatypes

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,11 @@
 let age = 19;
-number => 3456789
+number = 3456789
 check = false;
+null 
+undefined
+Symbol
+Object
+
+console.log(typeof null);
+console.log(typeof undefiend);
+console.log(typeof check);
