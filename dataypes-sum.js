78595cd0287ac0e1fd5365de3eb0b1846900b20f
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-20 15:37:49 +0530

    datatypes summery

---
+++ dataypes-sum.js
@@ -0,0 +1,17 @@
+// primitive datatypes
+
+// 7 types : String, Number, Boolean, null, undefiend, Symbol, BigInt
+
+
+// Refernce (Non-prmitive)
+
+// Array,Objects,Functions
+
+
+const fun = function(){
+    console.log("Hello Everyone");
+}
+fun()
+
+const sy = Symbol("2345@#$6")
+console.log(typeof sy);
