d86dfc8da67ffd5b2966f52e94fa010abe7e92b5
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-20 15:06:24 +0530

    operations in js

--- operations.js
+++ operations.js
@@ -1,7 +1,39 @@
-let someNum = 44
+// let someNum = 44
 
-let stringNum = String(someNum)
-console.log(String(someNum));
+// let stringNum = String(someNum)
+// console.log(String(someNum));
 
-console.log(typeof stringNum);
-console.log(stringNum);
+// console.log(typeof stringNum);
+// console.log(stringNum);
+
+// let num = 20;
+// console.log(-num);
+
+let s1 = "2"
+let s2 = "6"
+
+// console.log(s1+s2);
+
+// console.log("s1"+s2);
+// console.log(s1+"s2");
+// console.log(s2+"2"+s1);
+// console.log(2+"s1"+s2);
+
+// console.log(+"");
+// console.log(+true);
+// console.log(-" ");
+
+// let n1,n2,n3
+
+// let n1=n2=n3 = 144+12
+// console.log(n3);
+// console.log(n1);
+// console.log(n2);
+
+let count=10
+console.log(count++);
+console.log(++count);
+
+
+
+
