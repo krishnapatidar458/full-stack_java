8d5e37f1a40151328d2d36c8a41f37fcd29b4951
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-20 16:58:36 +0530

    learned string predefiend method

---
+++ string.js
@@ -0,0 +1,26 @@
+const  name = "krishna"
+const count = 235
+
+// console.log(name+count+" this is string concatinasion" );
+
+// console.log(`Hello, my name is ${name.toUpperCase()} and the given count is ${count} .`);
+
+const Name = new String('krishna patidar')
+
+
+// console.log("\n" + Name[0]);
+// console.log(Name.__proptp__);
+
+// console.log(Name.toUpperCase());
+console.log(Name.charAt(13));
+console.log(Name.indexOf('p'));
+
+console.log(Name.substring(8,15));
+
+console.log(Name.slice(-15,8));
+
+//Name.trim()
+
+console.log(Name.includes('patidar'));
+
+console.log(Name.split(' '));
