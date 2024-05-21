295c815ea6714041f0810ac9fd16af85f41478cc
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-21 16:39:25 +0530

    try and catch in exception in java

--- Spring & Spring Boot/Basic java/Exceptions.java
+++ Spring & Spring Boot/Basic java/Exceptions.java
@@ -1,8 +1,13 @@
 
-
-
 class Exceptions{
     public static void main(String args[]){
-
+        int i=2,j=0;
+        try{
+            i=i/j;
+        }
+        catch(Exception e){
+            System.out.println("somthing went wrong");
+        }
+        System.out.println(i+" See you soon.");
     }
 }
