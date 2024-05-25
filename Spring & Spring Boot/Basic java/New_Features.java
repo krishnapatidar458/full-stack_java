b9faab14c25d8b79ffc10da9312b1a636927337a
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-05-25 10:19:26 +0530

    updates in new features using java

--- Spring & Spring Boot/Basic java/New_Features.java
+++ Spring & Spring Boot/Basic java/New_Features.java
@@ -8,46 +8,46 @@ sealed interface x permits y{}
 non-sealed interface y extends x{}
 
 // class Alien {
-    private final int id;
-    private final String name;
+//     private final int id;
+//     private final String name;
 
-    public  Alien(int id,String name){
-        this.id = id;
-        this.name = name;
-    }
+//     public  Alien(int id,String name){
+//         this.id = id;
+//         this.name = name;
+//     }
 
-    public int getId(){
-        return id;
-    }
+//     public int getId(){
+//         return id;
+//     }
 
-    public String getName(){
-        return name;
-    }
+//     public String getName(){
+//         return name;
+//     }
 
 
-    @Override
-    public boolean equals(Object o){
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Alien alien = (Alien) o;
-        return id == alien.id && Objects.equals(name, alien.name);
-    }
+//     @Override
+//     public boolean equals(Object o){
+//         if (this == o) return true;
+//         if (o == null || getClass() != o.getClass()) return false;
+//         Alien alien = (Alien) o;
+//         return id == alien.id && Objects.equals(name, alien.name);
+//     }
 
-    @Override
-    public String toString(){
-        return "Alien{"+ 
-        "id=" + id + 
-        ", name'" + name + 
-        '\'' + 
-        '}';
-    }
+//     @Override
+//     public String toString(){
+//         return "Alien{"+ 
+//         "id=" + id + 
+//         ", name'" + name + 
+//         '\'' + 
+//         '}';
+//     }
 
-    @Override
-    public int hashCode(){
-        return Objects.hash(id, name);
-    }
+//     @Override
+//     public int hashCode(){
+//         return Objects.hash(id, name);
+//     }
 
-}
+// }
 
 record Alien(int id,String name){ }
 public class New_Features {
@@ -56,7 +56,7 @@ public class New_Features {
         // var b = 10;
         // System.out.println(a+b);
 
-        Alien obj = new Alien(1, "krishna");
+        // Alien obj = new Alien(1, "krishna");
         Alien obj1 = new Alien(1, "krishna");
         // Alien obj1 = new Alien(2, "Patidar");
 
