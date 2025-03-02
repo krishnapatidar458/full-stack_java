527fbc691e967910423deadd8c3c523732362787
krishnapatidar458 <kp7patidardudawat@gmail.com>
2025-03-02 17:50:52 +0530

    testing done

--- Spring & Spring Boot/FirstWebApp/src/main/java/com/SimpleWebApp/FirstWebApp/Controllers/HomeController.java
+++ Spring & Spring Boot/FirstWebApp/src/main/java/com/SimpleWebApp/FirstWebApp/Controllers/HomeController.java
@@ -1,11 +1,15 @@
 package com.SimpleWebApp.FirstWebApp.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HomeController {
     @RequestMapping
+    @ResponseBody
     public String message(){
         return "Welcome in Spring World";
     }
--- Spring & Spring Boot/FirstWebApp/src/main/java/com/SimpleWebApp/FirstWebApp/Controllers/LoginController.java
+++ Spring & Spring Boot/FirstWebApp/src/main/java/com/SimpleWebApp/FirstWebApp/Controllers/LoginController.java
@@ -1,4 +1,14 @@
 package com.SimpleWebApp.FirstWebApp.Controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String Login(){
+        return "Login Page..";
+    }
 }
